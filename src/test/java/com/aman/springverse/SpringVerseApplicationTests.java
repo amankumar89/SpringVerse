10708d720bbbe145b1b86af42a4f25657e565fb1
amankumar89 <amankumaroo784@gmail.com>
2026-07-14 11:30:23 +0530

    initial setup

---
+++ src/test/java/com/aman/springverse/SpringVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.springverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVerseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
