8d6baa80b51e281a778263aef343f2640e5af774
amankumar89 <amankumaroo784@gmail.com>
2026-07-14 12:30:37 +0530

    added course enum

---
+++ src/main/java/com/aman/springverse/modules/students/entity/CourseEnum.java
@@ -0,0 +1,26 @@
+package com.aman.springverse.modules.students.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum CourseEnum {
+    JAVA("JAVA"),
+    CORE_JAVA("Core Java"),
+    SPRING("Spring"),
+    SPRING_BOOT("Spring Boot"),
+    MERN("MERN"),
+    REACT("React.js"),
+    NODE("Node.js"),
+    EXPRESS("Express.js"),
+    TYPESCRIPT("TypeScript"),
+    JAVASCRIPT("JavaScript"),
+    REDIS("Redis"),
+    ;
+
+    private final String displayName;
+
+    CourseEnum(String displayName) {
+        this.displayName = displayName;
+    }
+
+}
