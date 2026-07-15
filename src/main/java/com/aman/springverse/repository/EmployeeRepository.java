f3fac854af296c924d8eb0878a03606d0b55f248
amankumar89 <amankumaroo784@gmail.com>
2026-07-15 12:52:45 +0530

    added employee department enum and entity, repository class

---
+++ src/main/java/com/aman/springverse/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.aman.springverse.repository;
+
+import com.aman.springverse.entity.employees.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
+}
