8e24bf699f92111e55c51da6d5cfcb670d47b21e
amankumar89 <amankumaroo784@gmail.com>
2026-07-18 20:20:26 +0530

    added no content api respone and added some methods in jpa repository

--- src/main/java/com/aman/springverse/repository/TaskRepository.java
+++ src/main/java/com/aman/springverse/repository/TaskRepository.java
@@ -1,7 +1,13 @@
 package com.aman.springverse.repository;
 
+import com.aman.springverse.entity.tasks.Status;
 import com.aman.springverse.entity.tasks.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    boolean existsByTitle(String title);
+
+    List<Task> findByStatusNot(Status status);
 }
