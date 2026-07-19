a7775b941306770e0984c6773fd24404a8b82f5e
amankumar89 <amankumaroo784@gmail.com>
2026-07-19 13:10:03 +0530

    added products repository

---
+++ products/src/main/java/com/aman/products/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.aman.products.repository;
+
+import com.aman.products.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
